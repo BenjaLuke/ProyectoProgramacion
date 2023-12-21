db97cc299e04e5558b53651a1d0c9c6364c7669e
lvalbuena77 <lvalbuena@alumnos.viu.es>
2023-12-21 17:43:16 +0100

    implementar facturacion y menuFacturacion

---
+++ src/main/java/menuFacturacion.java
@@ -0,0 +1,111 @@
+// 21GIIN - Proyectos de Programación - Clase menuFacturacion
+// Proyecto: Gestión logística Portuaria
+// Autores: Luis Valbuena - Benjamín Miguel
+
+import java.util.Calendar;
+import java.util.Scanner;
+import java.sql.Date;
+import java.sql.Time;
+
+// Clase que se encarga de gestionar el menú de facturación
+public class menuFacturacion { 
+
+    private static Date obtenerFechaActual() { // Método para obtener la fecha actual
+        Calendar calendar = Calendar.getInstance(); // Creamos un objeto Calendar para obtener la fecha actual
+        return new Date(calendar.getTimeInMillis()); // Devolvemos la fecha actual
+    }
+
+    private static Time obtenerHoraActual() { // Método para obtener la hora actual
+        Calendar calendar = Calendar.getInstance(); // Creamos un objeto Calendar para obtener la hora actual
+        return new Time(calendar.getTimeInMillis()); // Devolvemos la hora actual
+    }
+    
+    public static void iniciarMenu() { // Método para iniciar el menú de facturación
+        System.setProperty("file.encoding", "UTF-8"); // Establecemos la codificación de caracteres
+        Scanner scanner = new Scanner(System.in); // Creamos un objeto Scanner para leer datos del usuario
+        int opcion; // Variable para almacenar la opción seleccionada por el usuario
+
+        // Crear la conexión con la base de datos
+        SQLiteManager manager = new SQLiteManager("src/db/proyecto_logistica.db");
+        
+            
+        try { 
+
+            // Crear la tabla de facturación si no existe
+            manager.createTableFacturacion();
+
+            // Insertar datos si es necesario
+            // manager.insertData();
+
+            do {
+                System.out.println("=== Menú ===");
+                System.out.println("1. Crear factura");
+                System.out.println("2. Modificar factura");
+                System.out.println("3. Eliminar factura");
+                System.out.println("4. Mostrar todas las facturas");
+                System.out.println("5. Salir");
+                System.out.print("Seleccione una opción: ");
+                opcion = scanner.nextInt();
+                scanner.nextLine(); // Consumir el salto de línea después del nextInt()
+
+                switch (opcion) {
+                    case 1:
+                        crearFactura(scanner);
+                        break;
+                    case 2:
+                        modificarFactura(scanner);
+                        break;
+                    case 3:
+                        eliminarFactura(scanner);
+                        break;
+                    case 4:
+                        mostrarFacturas();
+                        break;
+                    case 5:
+                        System.out.println("Saliendo del programa...");
+                        break;
+                    default:
+                        System.out.println("Opción no válida. Intente de nuevo.");
+                }
+            } while (opcion != 5);
+
+        }finally{
+            manager.closeConnection(); // Cerrar la conexión con la base de datos
+        }
+        scanner.close();
+    }
+
+    private static void crearFactura(Scanner scanner) { // Método para crear una factura
+        System.out.println("=== Crear factura ===");
+
+        // Obtener la fecha y la hora actuales del sistema
+        java.sql.Date fechaActual = obtenerFechaActual();
+        java.sql.Time horaActual = obtenerHoraActual();
+
+        System.out.println("Fecha actual: " + fechaActual);
+        System.out.println("Hora actual: " + horaActual);
+
+        System.out.print("Introduzca el tipo de la factura: ");
+        String tipo = scanner.nextLine(); // Pedimos el tipo de la factura
+        System.out.print("Introduzca la matrícula del vehículo: ");
+        String matricula = scanner.nextLine(); // Pedimos la matrícula del vehículo
+        System.out.print("Introduzca el nombre del cliente: ");
+        String nombre = scanner.nextLine(); // Pedimos el nombre del cliente
+        System.out.print("Introduzca el DNI del cliente: ");
+        String dni = scanner.nextLine(); // Pedimos el DNI del cliente
+        System.out.print("Introduzca el concepto de la factura: ");
+        String concepto = scanner.nextLine(); // Pedimos el concepto de la factura
+        System.out.print("Introduzca el importe de la factura: ");
+        double importe = scanner.nextDouble(); // Pedimos el importe de la factura
+        scanner.nextLine(); // Consumir el salto de línea después del nextDouble()
+        System.out.print("Introduzca el estado de la factura: ");
+        String estado = scanner.nextLine(); // Pedimos el estado de la factura
+
+        // Añadir ID de la factura de forma secuencial
+        int id_facturacion = 1 + (int) (Math.random() * 100); // Generar ID aleatorio
+
+        Facturacion factura = new Facturacion(id_facturacion, fechaActual, horaActual, tipo, matricula, nombre, dni, concepto, importe, estado); // Creamos un objeto Facturacion con los datos introducidos por el usuario
+        factura.crear(); // Creamos la factura en la base de datos
+    }
+
+}
